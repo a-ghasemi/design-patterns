172323748b122080aa1f5ccd725e0a9a80522c68
Armin Ghasemi <datarivers.org@gmail.com>
2022-04-19 21:55:46 +0300

    Adds adapter DP

---
+++ B.Structural/01_Adapter/Concept/Structure/Target.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Structure;
+
+/**
+ * The Target defines the domain-specific interface used by the client code.
+ */
+class Target
+{
+    public function request(): string
+    {
+        return "Target: The default target's behavior.";
+    }
+}
